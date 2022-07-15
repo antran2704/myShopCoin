39f33ef6a4a4fa36dca89e5b39d2d874ce02f4da
antran2704 <phamtrangiaan27@gmail.com>
2022-07-15 21:22:55 +0700

    add feature

--- src/components/NavbarMobile/NavbarMobile.jsx
+++ src/components/NavbarMobile/NavbarMobile.jsx
@@ -3,35 +3,48 @@ import {BsPersonLinesFill} from "react-icons/bs"
 import { RiHandCoinLine } from "react-icons/ri";
 import { ImProfile } from "react-icons/im";
 import "./NavbarMobile.scss";
+import { Link } from "react-router-dom";
 
 function NavbarMobile() {
   return (
     <div className="navmobile">
       <ul className="navmobile__list">
-        <li className="navmobile__item">
-          <BiStoreAlt className="navmobile__icon" />
-          <p className="navmobile__name">Buy Coins</p>
-        </li>
-        <li className="navmobile__item">
-          <RiHandCoinLine className="navmobile__icon" />
-          <p className="navmobile__name">My Coins</p>
-        </li>
-        <li className="navmobile__item">
-          <BiHistory className="navmobile__icon" />
-          <p className="navmobile__name">History</p>
-        </li>
-        <li className="navmobile__item">
-          <BiMoney className="navmobile__icon" />
-          <p className="navmobile__name">Deposit</p>
-        </li>
-        <li className="navmobile__item">
-          <BsPersonLinesFill className="navmobile__icon" />
-          <p className="navmobile__name">Withdraw</p>
-        </li>
-        <li className="navmobile__item">
-          <ImProfile className="navmobile__icon" />
-          <p className="navmobile__name">My Profile</p>
-        </li>
+       <a href= "/">
+          <li className="navmobile__item">
+            <BiStoreAlt className="navmobile__icon" />
+            <p className="navmobile__name">Buy Coins</p>
+          </li>
+       </a>
+        <Link to = "/my-coin">
+          <li className="navmobile__item">
+            <RiHandCoinLine className="navmobile__icon" />
+            <p className="navmobile__name">My Coins</p>
+          </li>
+        </Link>
+        <Link to = "/my-history">
+          <li className="navmobile__item">
+            <BiHistory className="navmobile__icon" />
+            <p className="navmobile__name">History</p>
+          </li>
+        </Link>
+        <Link to = "/my-draw">
+          <li className="navmobile__item">
+            <BiMoney className="navmobile__icon" />
+            <p className="navmobile__name">Deposit</p>
+          </li>
+        </Link>
+       <Link to= "/my-draw">
+          <li className="navmobile__item">
+            <BsPersonLinesFill className="navmobile__icon" />
+            <p className="navmobile__name">Withdraw</p>
+          </li>
+       </Link>
+        <Link to = "/my-coin">
+          <li className="navmobile__item">
+            <ImProfile className="navmobile__icon" />
+            <p className="navmobile__name">My Profile</p>
+          </li>
+        </Link>
       </ul>
     </div>
   );
